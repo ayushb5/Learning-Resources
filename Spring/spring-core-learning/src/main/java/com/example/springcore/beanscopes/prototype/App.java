bae73c7808f2c6dae16ce140d8df7e46353e71fe
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-09-20 12:24:41 +0530

    added important comments inside files for scope related concept

--- Spring/spring-core-learning/src/main/java/com/example/springcore/beanscopes/prototype/App.java
+++ Spring/spring-core-learning/src/main/java/com/example/springcore/beanscopes/prototype/App.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
 	public static void main(String[] args) {
 //		Prototype scope creates a new Bean instance every time the Bean is requested from the Spring Container.
+//		In lazy initialization, Spring creates the Bean only when it is first requested or needed.
+//		Default for prototype scope is lazy initialization
+		
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		Employee e1 = context.getBean(Employee.class);
 		Employee e2 = context.getBean(Employee.class);
--- Spring/spring-core-learning/src/main/java/com/example/springcore/beanscopes/singleton/App.java
+++ Spring/spring-core-learning/src/main/java/com/example/springcore/beanscopes/singleton/App.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
 	public static void main(String[] args) {
 //		Singleton is the default Spring Bean scope in which Spring creates one Bean instance per Spring IoC Container.
-
+//		In eager initialization, Spring creates the Bean when the ApplicationContext starts.
+//
+//		Default for singleton Beans.
+//		Bean is created before it is explicitly requested.
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		EmployeeService e1 = context.getBean(EmployeeService.class);
 		EmployeeService e2 = context.getBean(EmployeeService.class);
